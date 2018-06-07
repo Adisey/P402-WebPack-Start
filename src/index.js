426eec0d117da08aeb738224815f34eadc055055
Andrey Smirnov <iadisey@gmail.com>
2018-06-07 17:35:48 +0300

    Commit changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import moment from 'moment';
 
+moment.locale('ru');
 function component() {
     let element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    moment.locale('ru');
+    element.innerHTML = _.join(['Hello', 'webpack', moment().format('LLLL')], ' ');
 
     return element;
 }
