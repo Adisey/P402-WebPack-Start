8cff9ceda888f9bcd408bba34fba8fba2246056d
Andrey Smirnov <iadisey@gmail.com>
2018-06-07 23:16:06 +0300

    Упражнение закончено. Начинеаем новое упражнение.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,45 @@
-var path = require('path');
+const path = require ('path');
+const autoprefixer = require ('autoprefixer');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve (__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[
+                        {loader: 'style-loader'},
+                        {loader: 'css-loader',
+                            options: {sourceMap: true}
+                        },
+                        {loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    autoprefixer ({
+                                        browsers: ['ie >= 8', 'last 4 version']
+                                    })
+                                ],
+                                sourceMap: true
+                            }
+                        },
+                        {loader: 'sass-loader',
+                            options: {
+                                includePaths: [
+                                    helpers.root ('src', 'styles', 'global'),
+                                ],
+                                sourceMap: true
+                            }
+                        }
+                    ]
+
+            }
+        ]
+    }
+
+
+}
+;
